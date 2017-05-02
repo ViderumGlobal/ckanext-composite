5c4791d0f70a049eb48287f2015374ef40a16197
kmbn <github@nevermindtheumlauts.com>
2017-05-02 17:02:11 +0200

    Localize date inserted into form field

--- ckanext/composite/fanstatic/datepicker.js
+++ ckanext/composite/fanstatic/datepicker.js
@@ -1,15 +1,22 @@
 var dates = $('.scheming-datepicker');
 var dateOptions;
-var currentLang;
+var currentLang = $('html').attr('lang') || 'en';
+var dateFormat;
+
+// Set dateFormat
+if (currentLang == 'de') {
+    dateFormat = 'DD.MM.YYYY';
+} else {
+    dateFormat = 'DD/MM/YYYY';
+}
 
 // Create datepicker instance for every date field
 if (dates) {
     dateOptions = {
         time: false,
         weekStart: 1,
-        inputFormat: 'DD.MM.YYYY'
+        inputFormat: dateFormat
     };
-    currentLang = $('html').attr('lang') || 'en';
 
     moment.locale(currentLang);
 
